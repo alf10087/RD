468cd01e793c810f069dce3a2d84b568f72b6c16
Alfonso Rojas-Alvarez <rojas.alfonso@me.com>
2017-09-21 14:08:54 -0500

    Add files via upload

--- Assignment 2.R
+++ Assignment 2.R
@@ -15,6 +15,8 @@ library(easyGgplot2)
 library(splitstackshape)
 library(dplyr)
 library(plyr)
+library(survey)
+library(sampling)
 rm(list = ls())
 
 ######## MODIFY THIS TO YOUR PATH
@@ -50,4 +52,35 @@ sd(combined$sis)
 
 strat_sample1 <- stratified(combined, c("gkschid"), size = 2) 
 
+#### Create a boxplot just to take a look at them
 
+boxplot(strat_sample1$gktreadss ~ strat_sample1$gkschid, ylab = "Mean Reading Score", xlab = "School ID")
+
+#### Set Survey Design Parameters for Stratified Sample
+
+### First Sample
+
+design_sample1 <- svydesign(id = ~1, strata = ~gkschid, data = strat_sample1, weights= ~sis)
+
+svymean(~gktreadss, design = design_sample1)
+confint(svymean(~gktreadss, design = design_sample1))
+
+### Second Sample
+
+strat_sample2 <- stratified(combined, c("gkschid"), size = 2) 
+
+design_sample2 <- svydesign(id = ~1, strata = ~gkschid, data = strat_sample2, weights= ~sis)
+
+svymean(~gktreadss, design = design_sample2)
+confint(svymean(~gktreadss, design = design_sample2))
+
+#### Cluster Madness
+
+cl <- cluster(combined, clustername=c("gkschid"), size = 10)
+sample3 <- getdata(combined, cl)
+
+strat_sample3 <- stratified(sample3, c("gkschid"), size = 16) 
+design_sample3 <- svydesign(id = ~1, strata = ~gkschid, data = strat_sample3, weights= ~sis)
+
+svymean(~gktreadss, design =design_sample3)
+confint(svymean(~gktreadss, design =design_sample3))
